d699d78e55b6bd25f03a3dfda913413103f35154
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-06-13 11:36:49 +0530

    feat:add auth routes

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,38 @@
+import { getAuth } from "@clerk/express";
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/appError";
+import { User } from "../models/User";
+import { asyncHandler } from "../utils/asyncHandler";
+
+export function requireAuth(req: Request, _res: Response, next: NextFunction) {
+  const { userId } = getAuth(req);
+  if (!userId) {
+    return next(
+      new AppError(401, "User is not logged in.Means unauthenticated user"),
+    );
+  }
+  next();
+}
+
+export async function getDbUserFromReq(req: Request) {
+  const { userId } = getAuth(req);
+  if (!userId) {
+    throw new AppError(401, "User is not logged in.Means unauthenticated user");
+  }
+  const dbUser = await User.findOne({ clerkUserId: userId });
+  if (!dbUser) {
+    throw new AppError(404, "User is not found in the DB");
+  }
+  return dbUser;
+}
+
+//Admin gate (user loggedIn + admin access)
+export const requireAdmin = asyncHandler(
+  async (req: Request, _res: Response, next: NextFunction) => {
+    const extractCurrentDbUser = await getDbUserFromReq(req);
+    if (extractCurrentDbUser.role !== "admin") {
+      throw new AppError(403, "Admin access only");
+    }
+    next();
+  },
+);
